8ffd75e1257b5b6da5fd9ef46ea013326080e5cd
Ahmed Khaled <ahmed23121998@gmail.com>
2025-03-18 12:18:20 +0200

    Ahmed changes

--- src/components/gitcomponent/New.jsx
+++ src/components/gitcomponent/New.jsx
@@ -6,11 +6,11 @@ function New() {
     <div>New</div>
     <p>new component</p>
     <div
-        class="alert alert-primary"
+        className="alert alert-primary"
         role="alert"
     >
-        <strong>Warning!</strong
-        ><a href="#" class="alert-link">Click Here</a>
+        <strong>Warning!</strong>
+        <a href="#" className="alert-link">Click Here</a>
     </div>
     
     
